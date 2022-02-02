c0b72e67540c0f63fdab41e733516b5da66f85ba
Mahii <90246070+mahiiverse1@users.noreply.github.com>
2022-02-02 22:25:09 +0530

    Create search.js

---
+++ Week-0-Challenge/webpages/search.js
@@ -0,0 +1,7 @@
+let query = document.querySelector('.query');
+        let button = document.querySelector('.button');
+
+        button.onclick = function() {
+            let url = 'https://www.google.com/search?q=' + query.value;
+            window.open(url);
+        }
